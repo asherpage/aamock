af33cf83769854b585b6b73bb8bc3b6d821dca30
asherpage <110576723+asherpage@users.noreply.github.com>
2024-03-14 15:09:22 -0700

    contact pager

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import { IoShieldCheckmark } from "react-icons/io5";
 import { FaLock } from "react-icons/fa";
 import { IoExtensionPuzzleSharp } from "react-icons/io5";
 import { FaPhone } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 function App() {
   return (
@@ -127,7 +131,55 @@ function App() {
       </div>
     </div>
     </div>
-    <div className='contact-main'></div>
+    <div className='contact-main'>
+      <div className="contact-container">
+            <div className="contact-info">
+              <h3>Our Contact</h3>
+                <h2>Get In Touch With Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, dolor. Lorem ipsum dolor sit amet.
+                </p>
+                <div className='linkers'>
+                  <div className='address'>
+                <h1>Address</h1>
+                <p>12471 W Bernard Way, Phx, Arizona</p>
+                </div>
+                <div className='phoner'>
+                <h1>Contact</h1>
+                <p>Email: contact@example.com</p>
+                <p>Phone: +123456789</p>
+                </div>
+                </div>
+                <h1 className='social'>Follow Us</h1>
+                <FaTwitter className='contact-icons'/>
+                <FaFacebookSquare className='contact-icons'/>
+                <RiInstagramFill className='contact-icons'/>
+            </div>
+            <div className="contact-form">
+                <form action="#" method="POST">
+                    <div className="form-group">
+                        <label htmlFor="first-name">First Name:</label>
+                        <input type="text" id="first-name" name="first-name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="last-name">Last Name:</label>
+                        <input type="text" id="last-name" name="last-name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message:</label><br></br>
+                        <textarea id="message" name="message" rows="4" required></textarea>
+                    </div>
+                    <button type="submit" className='contact-submit'>Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div className='testimonial-main'>
+
+    </div>
     </>
   );
 }
